43f16ed41b8f4ee48a12c3aef42d23c6ed303949
codingbang <codingbang@gmail.com>
2019-01-08 17:23:02 +0900

    volunteerList

--- vongether/src/main/java/com/vongether/volunteer/controller/VolunteerController.java
+++ vongether/src/main/java/com/vongether/volunteer/controller/VolunteerController.java
@@ -1,9 +1,16 @@
 package com.vongether.volunteer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/volunteer")
 public class VolunteerController {
-
+  
+  @RequestMapping(value="/list.do", method=RequestMethod.GET)
+  public String testPage() {
+    return "volunteer/volunteerList.page";
+  }
 
 }
