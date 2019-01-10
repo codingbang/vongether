c897eae0cfa3666ee7eb92c02f54f7bdedf11880
codingbang <codingbang@gmail.com>
2019-01-10 18:36:51 +0900

    PageNavigation 삭제로 인한 임시 주석처리

--- vongether/src/main/java/com/vongether/common/service/CommonService.java
+++ vongether/src/main/java/com/vongether/common/service/CommonService.java
@@ -1,10 +1,6 @@
 package com.vongether.common.service;
 
-import java.util.Map;
-
-import com.vongether.common.util.PageNavigation;
-
 public interface CommonService {
 
-	PageNavigation makePageNavigation(Map<String,String> param);
+	//PageNavigation makePageNavigation(Map<String,String> param);
 }
